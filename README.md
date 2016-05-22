f61c50688365ecc76d4aa450e0524dd8e8a9a1fe
yolixtly <yolixtly_13@hotmail.com>
2016-05-22 19:49:56 -0400

    the app is completed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # thoughts
 A fancy list maker to apply basic LESS, Grid System of Bootstrap
 http://yolixtly.github.io/thoughts.
+
+I really Enjoy building this good looking app!
--- app/app.js
+++ app/app.js
@@ -1,25 +1,33 @@
 $(document).ready(function() {
 	
-var deleteIcon = '<span class="icon"><i class="fa fa-times" aria-hidden="true"></i></span>';
-var pendingIcon = '<span class="icon"><i class="fa fa-circle-thin"></i></span>';
-var doneIcon = '<span class="icon"><i class="fa fa-check" aria-hidden="true"></i></span>';
+  var deleteIcon = '<span class="icon deleteIcon"><i class="fa fa-times" aria-hidden="true"></i></span>';
+  var doneIcon = '<span class="icon doneIcon"><i class="fa fa-check" aria-hidden="true"></i></span>';
 
-function main (){
-	$('form').submit(function(event){ //this is an event listener
-		var input = $(event.target).find('input');
-		var comment = input.val();
+  function main (){
+  	$('form').submit(function(event){ //this is an event listener
+  		var input = $(event.target).find('input');
+  		var comment = input.val();
 
-		if (comment != "") {
-			var html = $('<li>').append(comment + deleteIcon  + pendingIcon);
-			html.prependTo('#comments');
-			input.val("");
-		}
+  		if (comment != "") {
+  			var html = $('<li>').append(comment + deleteIcon  + doneIcon);
+  			html.prependTo('#comments');
+  			input.val("");
+  		}
 
-		return false; //instead of placing the event.preventDefault();
-	});
+  		return false; //instead of placing the event.preventDefault();
+  	});
 
-}
-main();
+  }
+  main();
 
+  /*Delete Items */
 
+  $(document).on('click', '.deleteIcon', function(){
+  	$(this).closest('li').fadeOut(300);
+  });
+
+  /*check Items Items */
+  $(document).on('click', '.doneIcon', function(){
+  	$(this).closest('li').toggleClass('doneItem');
+  });
 });
